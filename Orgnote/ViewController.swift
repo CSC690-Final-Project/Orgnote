924d8026186d1cdd4288ddaab223ded2315b7405
xhuang8 <xhuang8@mail.sfsu.edu>
2018-11-27 18:28:38 -0800

    Initial Commit

---
+++ Orgnote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Orgnote
+//
+//  Created by XiaoQian Huang on 11/27/18.
+//  Copyright © 2018 XiaoQian Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
