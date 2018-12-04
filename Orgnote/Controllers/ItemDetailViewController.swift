cbe42953ef69fa7bcaefdc5b4e103ce487dbbd49
paul <paul@pauls-MacBook-Pro.local>
2018-12-04 00:08:03 -0800

    setup itemdetailview with controller

---
+++ Orgnote/Controllers/ItemDetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ItemDetailViewController.swift
+//  Orgnote
+//
+//  Created by paul on 12/3/18.
+//  Copyright © 2018 XiaoQian Huang. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
