9bed4cb9fe858409546be13e12950d44a595f734
xhuang8 <xhuang8@mail.sfsu.edu>
2018-12-05 11:59:47 -0800

    created new viewcontroller, extends two photo class, connect to the button

---
+++ Orgnote/ViewController/HomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeViewController.swift
+//  Orgnote
+//
+//  Created by XiaoQian Huang on 12/5/18.
+//  Copyright © 2018 XiaoQian Huang. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cameraButton(_ sender: UIButton) {
+        
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
